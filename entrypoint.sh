93f037a14f453ea8dfdec13e78925a808c2afcb4
blacktop <blacktop@users.noreply.github.com>
2015-01-02 11:08:50 -0700

    Small fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ sed -i "s;^elasticsearch:.*;elasticsearch: ${KIBANA_ES_URL};" "/opt/kibana-${KIB
 sed -i "s;^kibana_index:.*;kibana_index: ${KIBANA_INDEX};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
 
 exec /opt/elasticsearch-${ELASTICSEARCH_VERSION}/bin/elasticsearch -Des.http.port=9200
+exec /opt/logstash-${LOGSTASH_VERSION}/bin/logstash -e 'input { stdin { } } output { elasticsearch { host => localhost } }'
 exec /opt/kibana-${KIBANA_VERSION}/bin/kibana
