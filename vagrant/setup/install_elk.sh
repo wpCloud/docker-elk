7762913cd075e821e454204422913e862aafffd9
blacktop <blacktop@users.noreply.github.com>
2015-01-03 15:58:39 -0700

    Update elk installer

--- vagrant/setup/install_elk.sh
+++ vagrant/setup/install_elk.sh
@@ -9,6 +9,8 @@ export LOGSTASH_VERSION=1.4.2
 wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
 sudo add-apt-repository "deb http://packages.elasticsearch.org/logstash/1.4/debian stable main"
+sudo apt-get update && sudo apt-get install elasticsearch logstash -y
+sudo update-rc.d elasticsearch defaults 95 10
 curl -s https://download.elasticsearch.org/kibana/kibana/kibana-${KIBANA_VERSION}.tar.gz | tar zx -C /opt
-logstash -e 'input { stdin { } } output { elasticsearch { host => localhost protocol => "http"} }' &
+/opt/logstash/bin/logstash -e 'input { stdin { } } output { elasticsearch { host => localhost protocol => "http"} }' &
 /opt/kibana-${KIBANA_VERSION}/bin/kibana
